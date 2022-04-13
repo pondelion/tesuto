8aae7b81a91d6ffbdad25c1c710ecbfe3e7054c3
pondelion <programming.deve@gmail.com>
2022-04-13 12:37:39 +0900

    update

--- index.py
+++ index.py
@@ -1,6 +1,14 @@
+from typing import Optional
+import sqlite3
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+con = sqlite3.connect('./test.db')
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS item(id integer primary key autoincrement,name text)")
+con.close()
 
 
 @app.get("/")
@@ -8,6 +16,26 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items")
+def read_item(id: Optional[int] = None, name: Optional[str] = None):
+    con = sqlite3.connect('./test.db')
+    cur = con.cursor()
+    if id is None and name is None:
+        cur.execute("select * from item")
+    elif id is not None:
+        cur.execute("select * from item where id=:id", {"id": id})
+    else:
+        cur.execute("select * from item where name=:name", {"name": name})
+    records = cur.fetchall()
+    con.close()
+    return {'items': records}
+
+
+@app.post("/items")
+def register_item(name: str):
+    con = sqlite3.connect('./test.db')
+    cur = con.cursor()
+    cur.execute("insert into item (name) values (?)", (name,))
+    con.commit()
+    con.close()
+    return {'message': f'registered item {name}'}
